485f20d07f05e05c1d4e7ba772b4e4f94711c950
이건준 <gunjoun99@gmail.com>
2023-06-22 21:09:15 +0900

    OX퀴즈

---
+++ src/OX퀴즈.java
@@ -0,0 +1,30 @@
+class OX퀴즈 {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        int sequence = 0;
+
+        for(String cal : quiz){
+            String[] strArray = cal.split(" ");
+            int firstNum = Integer.parseInt(strArray[0]);
+            int secondNum = Integer.parseInt(strArray[2]);
+            String operator = strArray[1];
+            int result = 0;
+
+            switch(operator){
+                case "+":
+                    result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+            }
+            if(result == Integer.parseInt(strArray[4])){
+                answer[sequence] = "O";
+            } else{
+                answer[sequence] = "X";
+            }
+            sequence++;
+        }
+        return answer;
+    }
+}
