c6af82f09eac7bf715426c20c519dca1e68496bd
이건준 <gunjoun99@gmail.com>
2023-06-23 22:42:33 +0900

    다항식_더하기, 최빈값_구하기

---
+++ src/다항식_더하기.java
@@ -0,0 +1,56 @@
+class 다항식_더하기 {
+    public String solution(String polynomial) {
+        String answer = "";
+
+        String[] expression = polynomial.split(" ");
+        String withX = "";
+        String withoutX = "";
+
+        int x = 0;
+        int num = 0;
+
+        for(String term : expression){
+            if(term.contains("x")){
+                if(term.equals("x")){
+                    withX = withX + "1" + "x ";
+                } else {
+                    withX = withX + term + " ";
+                }
+            }else if(!term.contains("+")){
+                withoutX = withoutX + term + " ";
+            }
+        }
+
+        withX = withX.replace("x", "");
+
+        String[] withXArr = withX.split(" ");
+        String[] withoutXArr = withoutX.split(" ");
+
+        for(String xnum : withXArr){
+            if(xnum != ""){
+                x = x + Integer.parseInt(xnum);
+            }
+        }
+        for(String normalNum : withoutXArr){
+            if(normalNum != ""){
+                num = num + Integer.parseInt(normalNum);
+            }
+        }
+        if(num == 0){
+            if(x == 1){
+                answer = "x";
+            } else {
+                answer = x + "x";
+            }
+        } else if(x == 0){
+            answer = Integer.toString(num);
+        } else {
+            if(x == 1){
+                answer = "x" + " + " + num;
+            } else{
+                answer = x + "x" + " + " + num;
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/최빈값_구하기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class 최빈값_구하기 {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        int[] result = new int[1001];
+
+        for(int num : array){
+            result[num] = result[num] + 1;
+        }
+
+        int max = result[0];
+        int maxIndex = 0;
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] > max) {
+                max = result[i];
+                maxIndex = i;
+            }
+        }
+
+        answer = maxIndex;
+
+        Arrays.sort(result);
+        if(result[999] == result[1000]){
+            answer = -1;
+        }
+        return answer;
+    }
+}
